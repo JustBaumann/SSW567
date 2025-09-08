24bb080b221f22825a9892e94d55013b019bb1ae
JustBaumann <95549617+JustBaumann@users.noreply.github.com>
2025-09-08 11:08:06 -0400

    Add files via upload
    Hello World Program HW 00A

---
+++ HW 00A.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Justin Baumann
+#9/8/2025
+#SSW567
+#I pledge my honor that I have abided by the Stevens Honor System
+
+
+# In[1]:
+
+
+print ("hello world")
+
